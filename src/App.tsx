a83e5eea1d9f2fa5a21399f263e3952804e42385
Vanitha Thangavelu <vanitha.thangavelu@roche.com>
2021-06-12 10:52:53 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,11 @@ import history from './routerHistory'
 // Only pool is included in the main bundle because of it's the most visited page
 const Home = lazy(() => import('./views/Home'))
 const Farms = lazy(() => import('./views/Farms'))
-const Lottery = lazy(() => import('./views/Lottery'))
-const Ifos = lazy(() => import('./views/Ifos'))
+// const Lottery = lazy(() => import('./views/Lottery'))
+// const Ifos = lazy(() => import('./views/Ifos'))
 const NotFound = lazy(() => import('./views/NotFound'))
-const Collectibles = lazy(() => import('./views/Collectibles'))
-const Teams = lazy(() => import('./views/Teams'))
+// const Collectibles = lazy(() => import('./views/Collectibles'))
+// const Teams = lazy(() => import('./views/Teams'))
 const Team = lazy(() => import('./views/Teams/Team'))
 const Profile = lazy(() => import('./views/Profile'))
 const TradingCompetition = lazy(() => import('./views/TradingCompetition'))
@@ -55,18 +55,19 @@ const App: React.FC = () => {
             <Route path="/pools">
               <Pools />
             </Route>
-            <Route path="/lottery">
-              <Lottery />
-            </Route>
-            <Route path="/ifo">
-              <Ifos />
-            </Route>
-            <Route path="/collectibles">
-              <Collectibles />
-            </Route>
-            <Route exact path="/teams">
-              <Teams />
-            </Route>
+            {/* <Route path="/lottery"> */}
+            {/*  <Lottery />
+            */}
+            {/* </Route> */}
+            {/* <Route path="/ifo"> */}
+            {/*  <Ifos /> */}
+            {/* </Route> */}
+            {/* <Route path="/collectibles"> */}
+            {/*  <Collectibles /> */}
+            {/* </Route> */}
+            {/* <Route exact path="/teams"> */}
+            {/*  <Teams /> */}
+            {/* </Route> */}
             <Route path="/teams/:id">
               <Team />
             </Route>
